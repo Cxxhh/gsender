17324162242b1aa4c5b8929db0d3f5d1848b5651
Wu Lizhuang <155533574+Cxxhh@users.noreply.github.com>
2025-10-21 08:13:31 +0800

    feat: localize workspace navigation surfaces

--- src/app/src/react-routes.tsx
+++ src/app/src/react-routes.tsx
@@ -3,6 +3,7 @@ import noop from 'lodash/noop';
 import { BiSolidCylinder } from 'react-icons/bi';
 
 import Workspace from './workspace';
+import i18n from 'app/lib/i18n';
 import { Config } from './features/Config';
 import Firmware from './features/Firmware';
 import KeyboardShortcuts from './features/Keyboard';
@@ -68,55 +69,69 @@ export const ReactRoutes = () => {
                         element={
                             <div className="p-4 fixed-content-area no-scrollbar">
                                 <p className="text-lg font-semibold mb-4 dark:text-white">
-                                    Choose a tool to get started...
+                                    {i18n._('Choose a tool to get started...')}
                                 </p>
 
                                 <div className="grid lg:grid-cols-3 grid-cols-2 gap-4 fixed-select-tool-area overflow-y-auto overflow-x-hidden">
                                     <ToolCard
-                                        title="Surfacing"
-                                        description="Flatten your wasteboard or other non-flat stock"
+                                        title={i18n._('Surfacing')}
+                                        description={i18n._(
+                                            'Flatten your wasteboard or other non-flat stock',
+                                        )}
                                         icon={GiFlatPlatform}
                                         link="/tools/surfacing"
                                     />
 
                                     <ToolCard
-                                        title="Rotary Surfacing"
-                                        description="Turn square material into round stock for rotary cutting"
+                                        title={i18n._('Rotary Surfacing')}
+                                        description={i18n._(
+                                            'Turn square material into round stock for rotary cutting',
+                                        )}
                                         icon={BiSolidCylinder}
                                         link="/tools/rotary-surfacing"
                                     />
 
                                     <ToolCard
-                                        title="Movement Tuning"
-                                        description="Ensure that each axis of your machine is moving accurately"
+                                        title={i18n._('Movement Tuning')}
+                                        description={i18n._(
+                                            'Ensure that each axis of your machine is moving accurately',
+                                        )}
                                         icon={TbRulerMeasure}
                                         link="/tools/movement-tuning"
                                     />
 
                                     <ToolCard
-                                        title="XY Squaring"
-                                        description="Get your CNC accurately aligned to make square cuts"
+                                        title={i18n._('XY Squaring')}
+                                        description={i18n._(
+                                            'Get your CNC accurately aligned to make square cuts',
+                                        )}
                                         icon={MdSquareFoot}
                                         link="/tools/squaring"
                                     />
 
                                     <ToolCard
-                                        title="Keyboard Shortcuts"
-                                        description="Set up keyboard shortcuts for easy navigation and control"
+                                        title={i18n._('Keyboard Shortcuts')}
+                                        description={i18n._(
+                                            'Set up keyboard shortcuts for easy navigation and control',
+                                        )}
                                         icon={FaKeyboard}
                                         link="/tools/keyboard-shortcuts"
                                     />
 
                                     <ToolCard
-                                        title="Gamepad"
-                                        description="Easy hand-held CNC control using pre-made or custom profiles"
+                                        title={i18n._('Gamepad')}
+                                        description={i18n._(
+                                            'Easy hand-held CNC control using pre-made or custom profiles',
+                                        )}
                                         icon={FaGamepad}
                                         link="/tools/gamepad"
                                     />
 
                                     <ToolCard
-                                        title="Old Firmware"
-                                        description="This is depreciated and used to be used for updating firmware"
+                                        title={i18n._('Old Firmware')}
+                                        description={i18n._(
+                                            'This is depreciated and used to be used for updating firmware',
+                                        )}
                                         icon={FaMicrochip}
                                         link="/tools/firmware"
                                     />
@@ -128,8 +143,10 @@ export const ReactRoutes = () => {
                         path="keyboard-shortcuts"
                         element={
                             <Page
-                                title="Keyboard Shortcuts"
-                                description="Configure your keyboard shortcuts for various actions"
+                                title={i18n._('Keyboard Shortcuts')}
+                                description={i18n._(
+                                    'Configure your keyboard shortcuts for various actions',
+                                )}
                                 withGoBackButton
                                 withFullPadding
                             >
@@ -141,7 +158,7 @@ export const ReactRoutes = () => {
                         path="movement-tuning"
                         element={
                             <Page
-                                title="Movement Tuning"
+                                title={i18n._('Movement Tuning')}
                                 withGoBackButton
                                 withFixedArea
                             >
@@ -153,7 +170,7 @@ export const ReactRoutes = () => {
                         path="squaring"
                         element={
                             <Page
-                                title="XY Squaring"
+                                title={i18n._('XY Squaring')}
                                 withGoBackButton
                                 withFixedArea
                             >
@@ -165,7 +182,7 @@ export const ReactRoutes = () => {
                         path="surfacing"
                         element={
                             <Page
-                                title="Wasteboard Surfacing"
+                                title={i18n._('Wasteboard Surfacing')}
                                 withGoBackButton
                                 withFixedArea
                             >
@@ -177,7 +194,7 @@ export const ReactRoutes = () => {
                         path="rotary-surfacing"
                         element={
                             <Page
-                                title="Rotary Surfacing"
+                                title={i18n._('Rotary Surfacing')}
                                 withGoBackButton
                                 withFixedArea
                             >
@@ -189,8 +206,8 @@ export const ReactRoutes = () => {
                         path="gamepad"
                         element={
                             <Page
-                                title="Gamepad"
-                                description="Manage your gamepad profiles here"
+                                title={i18n._('Gamepad')}
+                                description={i18n._('Manage your gamepad profiles here')}
                                 withGoBackButton
                                 withFixedArea
                             >
@@ -206,7 +223,7 @@ export const ReactRoutes = () => {
                         path="firmware"
                         element={
                             <Page
-                                title="Firmware (Legacy)"
+                                title={i18n._('Firmware (Legacy)')}
                                 withGoBackButton
                                 withFixedArea
                             >
@@ -274,19 +291,19 @@ export const ReactRoutes = () => {
                     path="tools"
                     element={
                         <>
-                            <RemoteWidget label="Probe">
+                            <RemoteWidget label={i18n._('Probe')}>
                                 <Probe />
                             </RemoteWidget>
-                            <RemoteWidget label="Macros">
+                            <RemoteWidget label={i18n._('Macros')}>
                                 <Macros />
                             </RemoteWidget>
-                            <RemoteWidget label="Spindle">
+                            <RemoteWidget label={i18n._('Spindle')}>
                                 <Spindle />
                             </RemoteWidget>
-                            <RemoteWidget label="Coolant">
+                            <RemoteWidget label={i18n._('Coolant')}>
                                 <Coolant />
                             </RemoteWidget>
-                            <RemoteWidget label="Rotary">
+                            <RemoteWidget label={i18n._('Rotary')}>
                                 <Rotary />
                             </RemoteWidget>
                         </>
