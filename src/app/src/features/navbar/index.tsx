cf5adbf91dd9b31ab49243399bbc30e8f729fe5b
Wu Lizhuang <155533574+Cxxhh@users.noreply.github.com>
2025-10-22 15:15:42 +0800

    fix: restore original i18n resource keys

--- src/app/src/features/navbar/index.tsx
+++ src/app/src/features/navbar/index.tsx
@@ -5,6 +5,7 @@ import cx from 'classnames';
 
 import Carve from './assets/Carve.svg';
 import { NavbarLink } from './components/NavbarLink.tsx';
+import i18n from 'app/lib/i18n';
 
 export const NavBar = () => {
     return (
@@ -15,18 +16,26 @@ export const NavBar = () => {
                 )}
             >
                 <div className="py-5 border-gray-400 border-r-2 dark:border-gray-700"></div>
-                <NavbarLink href="/" svg={Carve} label="Carve" />
+                <NavbarLink
+                    href="/"
+                    svg={Carve}
+                    label={i18n._('Carve')}
+                />
                 <NavbarLink
                     href="stats"
                     icon={IoSpeedometerOutline}
-                    label="Stats"
+                    label={i18n._('Stats')}
                 />
-                <NavbarLink href="tools" icon={RiToolsFill} label="Tools" />
+                <NavbarLink
+                    href="tools"
+                    icon={RiToolsFill}
+                    label={i18n._('Tools')}
+                />
 
                 <NavbarLink
                     href="configuration"
                     icon={FaTasks}
-                    label="Config"
+                    label={i18n._('Config')}
                 />
             </div>
         </>
